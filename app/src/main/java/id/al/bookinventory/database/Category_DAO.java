c6e39b8b4f85cbb183f16efdd2ba1d2b1856802e
salmanfarizi <farizi.salman4@gmail.com>
2017-04-25 13:16:02 +0700

    test

---
+++ app/src/main/java/id/al/bookinventory/database/Category_DAO.java
@@ -0,0 +1,56 @@
+package id.al.bookinventory.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import id.al.bookinventory.core.CoreApplication;
+import id.al.bookinventory.helper.SpinnerObject;
+import id.al.bookinventory.model.Category;
+
+public class Category_DAO {
+    private SQLiteDatabase catalogDB;
+    private dbHelper helper;
+    private String[] allColumns = {Reusable.COL_CATEGORY_ID, Reusable.COL_CATEGORY_NAME, Reusable.COL_CATEGORY_DESC};
+
+    public Category_DAO(Context context) {
+        helper = CoreApplication.getInstance().getDatabase();
+    }
+
+    public void open() throws SQLiteException {
+        catalogDB = helper.getWritableDatabase();
+    }
+
+    public void close() {
+        catalogDB.close();
+    }
+
+    public List<SpinnerObject> getGenres() {
+        List<SpinnerObject> listGenre = new ArrayList<SpinnerObject>();
+        Cursor cursor = null;
+        try {
+            cursor = catalogDB.query(Reusable.TABLE_CATEGORY, allColumns, null, null, null, null, null);
+            if (!cursor.moveToFirst()) {
+                return listGenre;
+            } else {
+                do {
+                    Category category = new Category(cursor);
+                    SpinnerObject obj = new SpinnerObject(category.getCategory_id(), category.getCategory_name());
+                    listGenre.add(obj);
+                } while (cursor.moveToNext());
+                return listGenre;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return listGenre;
+    }
+}
